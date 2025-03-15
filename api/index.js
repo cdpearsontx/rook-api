9fd27967b4db68e18c3438c66c1c864430c10345
cdpearsontx <pearsoncd@outlook.com>
2025-03-14 20:46:15 -0500

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,18 +1,29 @@
 const express = require('express');
 const { MongoClient } = require('mongodb');
 
-const uri = process.env.MONGODB_URI;
+// Connection URI
+const uri = "mongodb+srv://pearsoncd:VoOdhfvlGRVd3JzV@cluster0.mongodb.net/ai_memory?retryWrites=true&w=majority";
 
 // MongoDB Client
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 const app = express();
+const port = process.env.PORT || 3000;  // Default port is 3000
+
 app.use(express.json());
 
-// Connect to MongoDB once at startup
+// Default route for root path "/"
+app.get('/', (req, res) => {
+    res.send("Rook API is running!");
+});
+
+// Connect to MongoDB and then start server
 client.connect()
     .then(() => {
         console.log("MongoDB connected!");
+        app.listen(port, () => {
+            console.log(`Server is running on port ${port}`);
+        });
     })
     .catch((err) => {
         console.error("Failed to connect to MongoDB", err);
@@ -54,6 +65,3 @@ app.post('/api/messages', async (req, res) => {
         res.status(500).json({ message: "Error saving message." });
     }
 });
-
-// Export the app for Vercel
-module.exports = app;
