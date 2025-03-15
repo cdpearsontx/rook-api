71c4e31b86836a7f9f9ea7b462a19b9a5abb498c
cdpearsontx <pearsoncd@outlook.com>
2025-03-14 21:19:11 -0500

    Update hello.js

--- api/hello.js
+++ api/hello.js
@@ -1,3 +1,4 @@
-export default async function handler(req, res) {
-    res.status(200).json({ message: "API is working!" });
+export default function handler(req, res) {
+  res.status(200).json({ message: "Hello from the serverless function!" });
 }
+
